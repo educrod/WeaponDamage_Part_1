0e4dc10dcd640cc961a6f2868b2f7f70ac3dffc3
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-05-25 00:05:29 -0300

    Add project files.

---
+++ WeaponDamage_Part_1/ArrowDamage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeaponDamage_Part_1
+{
+    class ArrowDamage
+    {
+        private const decimal BASE_MULTIPLIER = 0.35M;
+        private const decimal MAGIC_MULTIPLIER = 2.5M;
+        private const decimal FLAME_DAMAGE = 1.25M;
+        private int roll;
+        private bool flaming;
+        private bool magic;
+
+        public ArrowDamage(int roll)
+        {
+            this.roll = roll;
+            CalculateDamage();
+        }
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value; CalculateDamage();
+            }
+        }
+
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value; CalculateDamage();
+            }
+        }
+
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value; CalculateDamage();
+            }
+
+        }
+
+        public int Damage { get; private set; }
+
+        private void CalculateDamage()
+        {
+            decimal baseDamage = Roll * BASE_MULTIPLIER;
+            if (Magic)
+            {
+                baseDamage = Roll * BASE_MULTIPLIER;
+            }           
+
+            if (Flaming)
+            {
+                Damage = (int)Math.Ceiling(baseDamage + FLAME_DAMAGE);
+            }
+            else
+            {
+                Damage = (int)Math.Ceiling(baseDamage);
+            }
+
+        }
+    }
+}
---
+++ WeaponDamage_Part_1/SwordDamage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeaponDamage_Part_1
+{
+    class SwordDamage
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+        private int roll;
+        private bool flaming;
+        private bool magic;
+
+        public SwordDamage(int roll)
+        {
+            this.roll = roll;
+            CalculateDamage();
+        }
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value; CalculateDamage();
+            }
+        }
+        
+        public bool Flaming
+        {
+            get { return flaming; }
+            set 
+            {
+                flaming = value; CalculateDamage();
+            }
+        }
+
+        public bool Magic
+        {
+            get { return magic; }
+            set 
+            {
+                magic = value; CalculateDamage();
+            }
+
+        }
+       
+        public int Damage { get; private set; }
+        
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+   
+            if (Magic)
+            {
+                magicMultiplier = 1.75M;
+            }
+
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            
+            if (Flaming)
+            {
+                Damage  += FLAME_DAMAGE;
+            }
+
+        }        
+
+    }
+}
